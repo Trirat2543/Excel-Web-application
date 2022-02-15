17d31ac54898ead3981f528aa72a629e17dbce4e
Trirat Arromrit <66780756+Trirat2543@users.noreply.github.com>
2022-02-16 03:35:10 +0700

    Delete application.py

--- application.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import streamlit as st
-import plotly.express as px
-from PIL import Image
-
-st.set_page_config(page_title = 'Survey Results')
-st.header('Survey Result 2021')
-st.subheader('Was the tutorial helpful?')
